87038921ee371d999bc4a001805dfce9ab5f4f80
rechadson <rechadson9@gmail.com>
2019-04-09 18:09:46 -0300

    Atualização de codigo

--- app/models/forms.py
+++ app/models/forms.py
@@ -2,20 +2,26 @@ from flask_wtf import FlaskForm
 from wtforms import *
 from wtforms.validators import DataRequired
 
+class FornecedorForm(FlaskForm):
+    cnpj = IntegerField("cnpj",validators=[DataRequired()])
+    fornecedor = StringField("fornecedor",validators=[DataRequired()])
+    email = StringField("email",validators=[DataRequired()])
+
 class LoginForm(FlaskForm):
     username = StringField("username",validators=[DataRequired()])
     password = PasswordField("password",validators=[DataRequired()])
 
-class Cliente(FlaskForm):
+class ClienteForm(FlaskForm):
     name = StringField("nome",validators=[DataRequired()])
     cpf = StringField("cpf",validators=[DataRequired()])
     endereco = StringField("endereco",validators=[DataRequired()])
 
-class Produto(FlaskForm):
+class ProdutoForm(FlaskForm):
     codigo = StringField("codigo",validators=[DataRequired()])
     Nome = StringField("Nome",validators=[DataRequired()])
     preco = StringField("preco",validators=[DataRequired()])
-class Orcamento(FlaskForm):
+
+class OrcamentoForm(FlaskForm):
     codigo_Produto = StringField("codigo_Produto",validators=[DataRequired()])
     cpf_cliente = StringField("cpf_cliente",validators=[DataRequired()])
     Quantidade = IntegerField("Quantidade",validators=[DataRequired()])
